3de908842a5f1e231791d2abfbe0ee2fdf36a33f
Jasmijnvg <jasmijngenesen@gmail.com>
2024-05-22 15:44:29 +0200

    first commit test

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        class CreditCard {
+            String nameOnCard;
+            String number;
+
+            public CreditCard(String nameOnCard, String number) {
+                this.nameOnCard = nameOnCard;
+                this.number = number;
+            }
+        }
+    }
+}
