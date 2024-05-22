2e3dfa47792e7b15a1a0f448470ca449763f0f69
Jasmijnvg <jasmijngenesen@gmail.com>
2024-05-22 15:47:55 +0200

    added code to Dogowner.java

--- src/Dogowner.java
+++ src/Dogowner.java
@@ -1,2 +1,9 @@
 public class Dogowner {
+    String name;
+    String namedog;
+
+    public Dogowner(String name, String namedog) {
+        this.name = name;
+        this.namedog = namedog;
+    }
 }
